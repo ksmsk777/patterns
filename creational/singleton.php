61db20e0ed842da87f242befce98d1df1e533511
ksmsk <ksmsk@ya.ru>
2026-02-17 06:19:50 +0300

    singleton added тест кирилицы

---
+++ creational/singleton.php
@@ -0,0 +1,39 @@
+<?php
+
+final class Connection
+{
+  private static ?self $instance = null;
+  private static string $name;
+  /**
+   * Get the value of name
+   */ 
+  public static function getName(): string
+  {
+    return self::$name;
+  }
+  
+  /**
+   * Set the value of name
+   *
+   * @return  self
+   */ 
+  public static function setName(string $name): void
+  {
+    self::$name = $name;
+  }
+
+  public static function getInstance() : self
+  {
+    if (self::$instance === null) {
+      self::$instance = new self();
+      
+    }
+    return self::$instance;
+  } 
+}
+
+
+$connection = Connection::getInstance();
+$connection::setName('Laravel');
+
+var_dump($connection::getName());
